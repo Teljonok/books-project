79e5509b1cea7ea97367b2030fa235e760eaa04d
alisa kosinov <alisa.kosinov@gmail.com>
2022-01-28 14:54:25 +0200

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BooksListComponent } from './books/books-list/books-list.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { BooksGuard } from './books/books.guard';
+import { WishListComponent } from './books/wish-list/wish-list.component';
+import { TabsNavComponent } from './books/tabs-nav/tabs-nav.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'welcome', component: WelcomePageComponent},
+  {canActivate: [BooksGuard], path: 'books', component: TabsNavComponent},
+  {canActivate: [BooksGuard], path: 'search', component: BooksListComponent},
+  {canActivate: [BooksGuard], path: 'favorites', component: WishListComponent},
+  {path: '', redirectTo: 'welcome'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+
+import { BooksModule } from './books/books.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BooksModule
   ],
   providers: [],
   bootstrap: [AppComponent]
